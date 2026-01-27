d3aa1a4fa0f9bbd27cb3ee6460fc66dea89f559f
Sheik Mohammed Muqbil <muqbil712@gmail.com>
2026-01-27 23:07:26 +0530

    Add files via upload

---
+++ modifiers/Nam.java
@@ -0,0 +1,10 @@
+public class Nam {
+final int x = 7;
+final double PI = 3.14;
+public static void main(String []args)
+{
+Nam myObj = new Nam();
+System.out.println(myObj.x);
+System.out.println(myObj.PI);
+}
+}
