162015adb82ee5e6e07d173623bf8aeac1b85dda
Sheik Mohammed Muqbil <muqbil712@gmail.com>
2026-02-01 12:20:00 +0530

    Add files via upload

---
+++ user-input/Tricky.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Tricky
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner (System.in);
+System.out.print("Enter any integer: ");
+int age = sc.nextInt();
+sc.nextLine();
+System.out.print("Enter your name: ");
+String name = sc.nextLine();
+System.out.println(age + " " + name);
+}
+}
+
