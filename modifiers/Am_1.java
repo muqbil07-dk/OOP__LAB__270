a185a64da831c3efbea6563bc14a374e277ad3f3
Sheik Mohammed Muqbil <muqbil712@gmail.com>
2026-02-06 00:00:18 +0530

    Refactor Student instantiation in Am_1.java

--- modifiers/Am_1.java
+++ modifiers/Am_1.java
@@ -1,14 +1,11 @@
-class Student
-{
+class Student {
 public String rollno = "CH.SC.U4CSE25270";
 public String place = "Karur";
 }
-public class Am_1
-{
-public static void main (String[] args)
-{
-Student m = new Student();
-System.out.println("Roll no:" + m.rollno);
-System.out.println("Place:" + m.place);
+public class Am_1 {
+public static void main(String[] args) {
+Student n = new Student();   
+System.out.println("Roll no: " + n.rollno);
+System.out.println("Place: " + n.place);
 }
 }
