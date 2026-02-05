ed65fcab70679b4849c9fbeaaff9f267ff482db1
Sheik Mohammed Muqbil <muqbil712@gmail.com>
2026-02-05 23:57:08 +0530

    Add files via upload

---
+++ modifiers/Am_1.java
@@ -0,0 +1,14 @@
+class Student
+{
+public String rollno = "CH.SC.U4CSE25270";
+public String place = "Karur";
+}
+public class Am_1
+{
+public static void main (String[] args)
+{
+Student m = new Student();
+System.out.println("Roll no:" + m.rollno);
+System.out.println("Place:" + m.place);
+}
+}
